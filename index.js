fc865c3073a398e4f3d5646c9c7f48e3e8f57567
github username <github email address>
2022-06-07 20:17:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+console.log(document)
+
+let element=document.querySelector("#main")
+element.remove();
+
+
+let newHeader=document.createElement("h1")
+
+newHeader.id="victory"
+newHeader.innerHTML="Caitlin is the champion";
